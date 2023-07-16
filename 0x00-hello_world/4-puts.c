21a83797f0cac2f0d79ad997a5dbdcffabe18caf
RorisangMoeng <moengqueen@gmail.com>
2023-07-16 09:49:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline.
+ * Return: Always 0.
  */
 int main(void)
 {
