817e7c84fc06014b29e7ae3366e03e8aff5c59ec
RorisangMoeng <moengqueen@gmail.com>
2023-08-29 08:08:03 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_listint - prints elements of a listin_t list
- * @h: linked list to print 
+ * @h: linked list to print
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
